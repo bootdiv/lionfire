d5520bf070ead688ac8dc2f4c33306cd61ca68b6
Dipto Paul <bootstrapdiv@gmail.com>
2021-05-02 14:39:51 +0600

    Final update

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    // Video
+    var player = videojs('video', {
+        aspectRatio: '16:9'
+    });
+
+    // Navigation scrolls
+    $(".navbar-nav li a").on('click', function (event) {
+        $('.navbar-nav li').removeClass('active');
+        $(this).closest('li').addClass('active');
+        var $anchor = $(this);
+        var nav = $($anchor.attr('href'));
+        if (nav.length) {
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+
+            event.preventDefault();
+        }
+    });
+
+});
+
+// Sticky header
+$(window).scroll(function () {
+    if ($(window).scrollTop() >= 110) {
+        $('#header').addClass('fixed-header');
+    } else {
+        $('#header').removeClass('fixed-header');
+    }
+});
